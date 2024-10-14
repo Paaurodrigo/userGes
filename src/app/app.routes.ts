9c25b9e23370f1ec6aa7d04ccf46a82caf9e7755
Pau <paurodrigo7@gmail.com>
2024-10-14 15:51:54 +0200

    de momento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {UsuarioAdminRoutedComponent} from "./component/usuario/admin/routed/usuario.admin.routed.component/usuario.admin.routed.component.component";
+export const routes: Routes = [
+    {
+        path: 'admin/usuario/plist', component: UsuarioAdminRoutedComponent }
+];
---
+++ src/app/component/usuario/admin/routed/usuario.admin.routed.component/usuario.admin.routed.component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario.admin.routed.component',
+  templateUrl: './usuario.admin.routed.component.component.html',
+  styleUrls: ['./usuario.admin.routed.component.component.css'],
+  standalone: true
+})
+export class UsuarioAdminRoutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
